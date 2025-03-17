0f20c6645f8d199a78e9e9c6f4066fe3e2fad54b
Kevin Esneider Ocampo Osorio <145300425+kelvinocampo@users.noreply.github.com>
2025-03-17 22:16:36 +0000

    Add: Basic General Routes

--- app.ts
+++ app.ts
@@ -3,11 +3,18 @@ import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 10101;
-app.get("/", (request: Request, response: Response) => {
-response.status(200).send("Hello World");
-});
+
+app.use('/usuario');
+app.use('/admin');
+app.use('/vendedor');
+app.use('/comprador');
+app.use('/transportador');
+app.use('/producto');
+app.use('/compra');
+app.use('/chat');
+
 app.listen(PORT, () => {
-console.log("Servidor ejecutándose en el puerto: ", PORT);
+    console.log("Servidor ejecutándose en el puerto: ", PORT);
 }).on("error", (error: { message: string | undefined; }) => {
-throw new Error(error.message);
+    throw new Error(error.message);
 });
