160eb9fa1fab304d955c8acf241d9585efa55f6e
Mari20007 <161378496+Mari20007@users.noreply.github.com>
2025-03-24 16:26:49 -0500

    Update user.ts

--- routes/user.ts
+++ routes/user.ts
@@ -7,4 +7,17 @@ router.post('/login');
 router.post('/edit', verifyToken);
 router.post('/profile', verifyToken);
 
+export default router; import express from "express";
+import verifyToken from "../middleware/VerifyToken";
+import { updatePasswordController } from "../controllers/userController";
+
+const router = express.Router();
+
+router.post('/register');
+router.post('/login');
+router.post('/edit', verifyToken);
+router.post('/profile', verifyToken);
+router.patch('/password', verifyToken, updatePasswordController); // Nuevo endpoint
+
 export default router;
+
