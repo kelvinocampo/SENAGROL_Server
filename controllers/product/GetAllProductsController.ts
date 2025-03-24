2bce2bd513702d5e77c6f82319523e947dcdfb98
Mari20007 <161378496+Mari20007@users.noreply.github.com>
2025-03-24 16:42:58 -0500

    Update GetAllProductsController.ts

--- controllers/product/GetAllProductsController.ts
+++ controllers/product/GetAllProductsController.ts
@@ -1,1 +1,18 @@
+import { Request, Response } from "express";
+import db from "../../config/database";
 
+const getAllProducts = async (req: Request, res: Response) => {
+    try {
+        const [products] = await db.query("SELECT * FROM producto");
+
+        return res.status(200).json(products);
+    } catch (error) {
+        return res.status(500).json({ message: "Error al obtener productos" });
+    }
+};
+
+export default getAllProducts;
+
+//Obtiene todos los productos de la base de datos.
+
+//Devuelve la lista en formato JSON
