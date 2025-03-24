bf87dc5bf74fd54be992ab2fd8caa8c9a7a1eb1f
Mari20007 <161378496+Mari20007@users.noreply.github.com>
2025-03-24 16:30:14 -0500

    Update GetDiscountedProductsController.ts

--- controllers/product/GetDiscountedProductsController.ts
+++ controllers/product/GetDiscountedProductsController.ts
@@ -1,1 +1,15 @@
+import { Request, Response } from "express";
+import db from "../../config/database";
+
+const getDiscountedProducts = async (req: Request, res: Response) => {
+    try {
+        const [products] = await db.query("SELECT * FROM producto WHERE descuento > 0");
+
+        return res.status(200).json(products);
+    } catch (error) {
+        return res.status(500).json({ message: "Error al obtener productos con descuento" });
+    }
+};
+
+export default getDiscountedProducts;
 
