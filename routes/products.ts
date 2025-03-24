1e2771e48fa0dd0efdd443670f0bd58126ae53d4
Mari20007 <161378496+Mari20007@users.noreply.github.com>
2025-03-24 17:38:38 -0500

    Update products.ts

--- routes/products.ts
+++ routes/products.ts
@@ -1,13 +1,1 @@
-import express from "express";
-import getDiscountedProducts from "../controllers/product/GetDiscountedProductsController";
-import getAllProducts from "../controllers/product/GetAllProductsController"; // Nuevo controlador
-import getProductById from "../controllers/product/GetProductByIdController"; // Nuevo controlador
-
-const router = express.Router();
-
-router.get("/discount", getDiscountedProducts);
-router.get("/", getAllProducts); // Obtener todos los productos
-router.get("/:id", getProductById); // Obtener producto por ID
-
-export default router;
 
