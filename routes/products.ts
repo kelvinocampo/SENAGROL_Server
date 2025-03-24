4866d9b5b7dfbc4dfdbdb31ec005af5313788473
Mari20007 <161378496+Mari20007@users.noreply.github.com>
2025-03-24 16:34:30 -0500

    Update products.ts

--- routes/products.ts
+++ routes/products.ts
@@ -1,1 +1,9 @@
+import express from "express";
+import getDiscountedProducts from "../controllers/product/GetDiscountedProductsController";
+
+const router = express.Router();
+
+router.get("/discount", getDiscountedProducts);
+
+export default router;
 
