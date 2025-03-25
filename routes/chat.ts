777af6ebb03fbc8cedee3ea2476a5a6ca871a321
Kevin Esneider Ocampo Osorio <145300425+kelvinocampo@users.noreply.github.com>
2025-03-25 17:21:51 +0000

    Add: middleware DeleteChat

--- routes/chat.ts
+++ routes/chat.ts
@@ -6,10 +6,13 @@ import UpdateTextMessageMiddleware from "../middleware/Chat/UpdateTextMessage";
 import UpdateTextMessageController from "../controllers/Chat/UpdateTextMessage";
 import DeleteMessageMiddleware from "../middleware/Chat/DeleteMessage";
 import DeleteMessageController from "../controllers/Chat/DeleteMessage";
+import DeleteChatMiddleware from "../middleware/Chat/DeleteChat";
 const router = express.Router();
 
 // router.post('/:id_chat/message/text', verifyToken, textMessageMiddleware.validatorParams, textMessageMiddleware.validator, textMessageController);
 router.post('/:id_chat/message/text', textMessageMiddleware.validatorParams, textMessageMiddleware.validator, textMessageController);
+router.delete('/:id_chat', DeleteChatMiddleware.validatorParams, DeleteChatMiddleware.validator, );
+
 router.put('/:id_chat/message/:id_message', UpdateTextMessageMiddleware.validatorParams, UpdateTextMessageMiddleware.validator, UpdateTextMessageController);
 router.delete('/:id_chat/message/:id_message', DeleteMessageMiddleware.validatorParams, DeleteMessageMiddleware.validator, DeleteMessageController);
 
