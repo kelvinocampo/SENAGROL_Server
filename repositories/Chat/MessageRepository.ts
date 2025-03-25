8d5b59989d70fd8913995535e5e198390a184dff
kelvinocampo <kevinocampooso@gmail.com>
2025-03-24 20:39:53 -0500

    Add: Basic Delete Message

--- repositories/Chat/MessageRepository.ts
+++ repositories/Chat/MessageRepository.ts
@@ -53,6 +53,32 @@ class MessageRepository {
             throw error;
         }
     }
+
+    static async deleteMessage(id_user: number, id_message: number, id_chat: number) {
+        try {
+            const query = `
+            DELETE FROM message
+            WHERE id_user = ?
+            AND id_mensaje = ?
+            AND id_chat = ?
+        `;
+
+            // El resultado es un array donde el primer elemento contiene la información de la operación
+            const [result]: any = await db.execute(query, [
+                id_user, id_message, id_chat
+            ]);
+
+            // Verificar si se actualizó alguna fila
+            if (result.affectedRows === 0) {
+                throw new Error('No se encontró el mensaje para actualizar');
+            }
+
+            return { affectedRows: result.affectedRows };
+        } catch (error) {
+            console.error("Error en MessageRepository:", error);
+            throw error;
+        }
+    }
 }
 
 export default MessageRepository;
--- routes/chat.ts
+++ routes/chat.ts
@@ -4,10 +4,12 @@ import textMessageController from "../controllers/Chat/TextMessage";
 import textMessageMiddleware from "../middleware/Chat/TextMessage";
 import UpdateTextMessageMiddleware from "../middleware/Chat/UpdateTextMessage";
 import UpdateTextMessageController from "../controllers/Chat/UpdateTextMessage";
+import DeleteMessageMiddleware from "../middleware/Chat/DeleteMessage";
 const router = express.Router();
 
 // router.post('/:id_chat/message/text', verifyToken, textMessageMiddleware.validatorParams, textMessageMiddleware.validator, textMessageController);
 router.post('/:id_chat/message/text', textMessageMiddleware.validatorParams, textMessageMiddleware.validator, textMessageController);
 router.put('/:id_chat/message/:id_message', UpdateTextMessageMiddleware.validatorParams, UpdateTextMessageMiddleware.validator, UpdateTextMessageController);
+router.delete('/:id_chat/message/:id_message', DeleteMessageMiddleware.validatorParams, DeleteMessageMiddleware.validator, DeleteTextMessageController);
 
 export default router;
