a8b4ebffd4622373574411020c2e9e6820b90a90
Mari20007 <161378496+Mari20007@users.noreply.github.com>
2025-03-24 23:54:06 -0500

    Update app.ts

--- app.ts
+++ app.ts
@@ -1,20 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
 import bodyParser from 'body-parser';
-
-//import userRoutes from './routes/user';
-//import productRoutes from "./routes/product"; // Agregar rutas de productos
+import userRoutes from "./routes/user";
+import productRoutes from "./routes/productoRoutes"; // Agregar rutas de productos
 import IARoute from './routes/IA';
+//import "./config/configDB";
+
 dotenv.config();
 const app = express().use(bodyParser.json());
 const PORT = process.env.PORT || 10101;
 
-// app.use('/usuario', userRoutes);
+app.use('/usuario', userRoutes);
+
 // app.use('/admin');
 // app.use('/vendedor');
 // app.use('/comprador');
 // app.use('/transportador');
-// app.use('/producto');
+app.use('/producto', productRoutes);
 // app.use('/compra');
 // app.use('/chat');
 app.use('/IA', IARoute)
