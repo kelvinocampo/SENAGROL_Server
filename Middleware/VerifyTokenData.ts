42bf3db55d52d71f16d70a3574498244b0f4c009
Kevin Esneider Ocampo Osorio <145300425+kelvinocampo@users.noreply.github.com>
2025-04-25 19:02:16 +0000

    Change: Verify Role Logic AND to OR

--- Middleware/VerifyTokenData.ts
+++ Middleware/VerifyTokenData.ts
@@ -3,8 +3,6 @@ import { Request, Response, NextFunction, RequestHandler } from "express";
 const verifyRole = (requiredRoles: string[]): RequestHandler => {
     return (req: Request, res: Response, next: NextFunction) => {
         try {
-            console.log(req.body);
-            
             // Verificar que el usuario está autenticado
             if (!req.body.id_user) {
                 return res.status(401).json({ error: "Usuario no autenticado" });
@@ -12,11 +10,12 @@ const verifyRole = (requiredRoles: string[]): RequestHandler => {
 
             const { roles } = req.body;
 
-            // Verificar cada rol requerido
-            for (const role of requiredRoles) {
-                if (!roles.includes(role)) {
-                    return res.status(403).json({ error: "Acceso restringido" });
-                }
+            const hasRequiredRole = requiredRoles.some(role =>
+                roles.includes(role.trim().toLowerCase())
+            );
+
+            if (!hasRequiredRole) {
+                return res.status(403).json({ error: "Acceso restringido" });
             }
 
             // Si pasa todas las validaciones, continuar
