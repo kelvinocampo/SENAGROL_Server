78d1af83187d4497aa08cc350c1b30ca30f409f5
Kevin Esneider Ocampo Osorio <145300425+kelvinocampo@users.noreply.github.com>
2025-04-30 17:32:01 +0000

    Add: Type AZURE_CONTAINER_NAME

--- Config/azureStorage.ts
+++ Config/azureStorage.ts
@@ -3,12 +3,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const { AZURE_STORAGE_CONNECTION_STRING = "" } = process.env;
+type AZURE_CONTAINER_NAME = "usuario" | "producto";
 
 if (!AZURE_STORAGE_CONNECTION_STRING) {
     throw new Error("Falta la cadena de conexión de Azure Storage");
 }
 
-export const connectStorage = (AZURE_CONTAINER_NAME: string) => {
+export const connectStorage = (AZURE_CONTAINER_NAME: AZURE_CONTAINER_NAME) => {
     const blobServiceClient = BlobServiceClient.fromConnectionString(AZURE_STORAGE_CONNECTION_STRING);
     const containerClient = blobServiceClient.getContainerClient(AZURE_CONTAINER_NAME);
     return containerClient;
